a10f64977d162deeff9860bc784dba144e42dbc2
Michael Kurze <michael.kurze@aixigo.de>
2015-10-16 10:39:26 +0200

    release v1.1.0

--- dist/laxar_testing.js
+++ dist/laxar_testing.js
@@ -4112,6 +4112,8 @@ define( 'laxar/lib/loaders/paths',[
             o.schema = o.env.resolveRef(null, o.schema);
             if (o.schema) o.schema = o.schema[0];
           }
+
+          if (!o.schema.type) o.schema.type = 'object';
         }
 
         if (o.schema && o.schema.type) {
